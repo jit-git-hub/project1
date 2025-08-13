ab018cddb5ee7f8fdbd1f4b037fef48ea5bff8dc
jit-git-hub <m.jitenderreddy@gmail.com>
2025-08-13 15:07:35 +0530

    login controller new commit

--- webapp/src/main/java/com/example/webapp/LoginController.java
+++ webapp/src/main/java/com/example/webapp/LoginController.java
@@ -1,13 +1,24 @@
 package com.example.webapp;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/webapp")
+@RestController()
 public class LoginController {
 
     @RequestMapping("/login")
     public String login(){
         return "WELCOME";
     }
+    @RequestMapping("/checklogin")
+    public String ckeckLogin(@RequestParam String name){
+        if(name.equals("jithu")){
+            return "WELCOME";
+        }
+        else return "WRONG";
+
+    }
+
 }
