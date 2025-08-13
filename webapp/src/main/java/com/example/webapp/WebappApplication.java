4a860d5c625f8d8717f80532a7c0ffeb0fc2bb98
jit-git-hub <m.jitenderreddy@gmail.com>
2025-08-13 13:39:27 +0530

    second commit

---
+++ webapp/src/main/java/com/example/webapp/WebappApplication.java
@@ -0,0 +1,15 @@
+package com.example.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.net.SocketOption;
+import java.sql.SQLOutput;
+
+@SpringBootApplication
+public class WebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebappApplication.class, args);
+	}
+}
